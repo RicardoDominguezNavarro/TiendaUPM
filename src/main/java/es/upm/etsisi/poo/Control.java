e03c988582b1e4e6e614c8e2f25f66073a77e3a9
ferna <fernandezcarrascolaura7@gmail.com>
2025-09-24 19:47:03 +0200

    Echo

--- src/main/java/es/upm/etsisi/poo/Control.java
+++ src/main/java/es/upm/etsisi/poo/Control.java
@@ -1,4 +1,12 @@
 package es.upm.etsisi.poo;
 
+import java.util.Scanner;
+
 public class Control {
+
+    public static String echo(Scanner keyboard, String s) {
+        System.out.print(s);
+        return keyboard.nextLine();
+    }
+
 }
