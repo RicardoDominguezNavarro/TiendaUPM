80a5cec1f1ebdf216a3bb01080b20b54467e7805
ferna <fernandezcarrascolaura7@gmail.com>
2025-09-26 11:12:13 +0200

    Laura

--- src/main/java/es/upm/etsisi/poo/Product.java
+++ src/main/java/es/upm/etsisi/poo/Product.java
@@ -11,20 +11,20 @@ public class Product {
     public enum Category{
         MERCH, PAPELERIA, ROPA, LIBRO, ELECTRONICA
     }
-    /*
-    controlar excepciones
-     */
+
     public Product(int id, String name, double price, Category category) {
-        if(id <= 0) {
+        if(id <= 0) { //id mayor que 0
             throw new IllegalArgumentException("Id may not be negative!");
         }
-        if(name == null || name.isEmpty() || name.length() > 100) {
+        if(name == null || name.trim().isEmpty() || name.trim().length() > 100) {
+            //el nombre no sea null, quita los espacios en blanco para ver si está vacía y quita los
+            // espacios y cuenta caracteres para que no haya más de 100
             throw new IllegalArgumentException("Invalid name!");
         }
-        if(price <= 0.0) {
+        if(price <= 0.0) { //precio mayor que 0
             throw new IllegalArgumentException("Price may not be negative!");
         }
-        if(category == null) {
+        if(category == null) { //categoria no es null
             throw new IllegalArgumentException("Category may not be null!");
         }
         this.id = id;
